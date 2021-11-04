fc8ba570e76f0866d929ec9a879a53eba9ab9124
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-03 23:25:16 -0400

    search a horizontal or vertical written word from a letter matrix

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let str = '';
+    letters.forEach(ls => {
+      str += ls[i];
+    });
+    verticalJoin.push(str);
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
